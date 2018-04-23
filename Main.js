2097760ae82697b267a691ef8a00f41c65ac8f90
Edison Neto <ednetoali@gmail.com>
2018-04-23 16:09:13 -0300

    I literally forgot to put my name on the license header

--- Main.js
+++ Main.js
@@ -1,8 +1,9 @@
 /**
  * Author: Edison Neto (ednetoali@gmail.com)
+ *
  * MIT License
 
- * Copyright (c) [year] [fullname]
+ * Copyright (c) 2018 Edison Aguiar de Souza Neto
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
